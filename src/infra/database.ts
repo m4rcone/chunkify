11ff42348bedb880470c59caaead792476ae63be
Marcone Boff <157663360+m4rcone@users.noreply.github.com>
2025-04-29 00:55:01 +0000

    feat: add `compose` config with `database` service and `services` script

---
+++ src/infra/database.ts
@@ -0,0 +1,37 @@
+import { Client } from "pg";
+
+async function query(queryObject: string) {
+  let client: Client | undefined;
+
+  try {
+    client = await getNewClient();
+    const result = await client.query(queryObject);
+
+    return result;
+  } catch (error) {
+    console.log(`Erro: ${error}`);
+  } finally {
+    await client?.end();
+  }
+}
+
+async function getNewClient() {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: Number(process.env.POSTGRES_PORT),
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
+
+  await client.connect();
+
+  return client;
+}
+
+const database = {
+  query,
+  getNewClient,
+};
+
+export default database;
