51ff7f2db30f3df18df8e6a65659ccdbe5ffed8a
m4cone <boffmarcone@gmail.com>
2025-05-29 14:56:51 -0300

    feat: add `wait-for-postgres` script

---
+++ src/infra/scripts/wait-for-postgres.ts
@@ -0,0 +1,22 @@
+const { exec } = require("node:child_process");
+
+process.stdout.write("\n🔴 Aguardando o postgres aceitar conexões.");
+
+checkPostgres();
+
+function checkPostgres() {
+  exec(
+    "docker exec postgres-dev pg_isready --host localhost",
+    (error, stdout) => {
+      if (stdout.includes("accepting connections")) {
+        console.log("\nPostgres pronto e aceitando conexões!\n");
+
+        return;
+      }
+
+      process.stdout.write(".");
+
+      checkPostgres();
+    },
+  );
+}
